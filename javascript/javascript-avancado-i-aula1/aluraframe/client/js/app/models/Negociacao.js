5940b5d6c14198c10c2576305cd0d7fbef7bcf49
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-03-25 11:58:41 -0300

    Minha primeira versão

--- javascript/javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript/javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -5,21 +5,22 @@ class Negociacao {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(n1);
     }
 
-    obtemVolume() {
+    get volume() {
         return this._quantidade * this._valor;
     }
 
-    getData() {
+    get data() {
         return this._data;
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
 
-    getValor() {
+    get valor() {
         return this._valor
     }
 }
