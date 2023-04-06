66312428eac4541614345b1da243710caf223c2f
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-04-06 08:14:22 -0300

    versão final

--- javascript/aluramidi/main.js
+++ javascript/aluramidi/main.js
@@ -1,7 +1,43 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento === null) {
+        alert('Elemento não encontrado')
+    }
+
+    else {
+        elemento.play();
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = () => {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = (evento) => {
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = () => {
+        tecla.classList.remove('ativa');
+    }
+}
+
+
+
 
 
