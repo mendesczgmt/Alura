28730a0a76ddf8b4962dcdfd6ce8b4748a7d2cde
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-04-19 17:54:25 -0300

    versão final

--- javascript/mochila-de-viagem/js/main.js
+++ javascript/mochila-de-viagem/js/main.js
@@ -9,20 +9,28 @@ itens.forEach(element => {
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
 
-const nome = evento.target.elements['nome'];
-const quantidade = evento.target.elements['quantidade'];
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
 
-const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
- }
+    const existe = itens.find(elemento => elemento.nome === nome.value);
 
- itens.push(itemAtual);
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
 
- localStorage.setItem("itens", JSON.stringify(itens));
+    if(existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+    }
+    else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id +1 : 0;
+        criarElemento(itemAtual);
+        itens.push(itemAtual);
+    }
 
-    criarElemento(itemAtual);
-    
+    localStorage.setItem("itens", JSON.stringify(itens));    
     nome.value = "";
     quantidade.value = "";
 })
@@ -34,9 +42,30 @@ function criarElemento(item) {
 
  const numeroIntem = document.createElement("strong");
  numeroIntem.innerHTML = item.quantidade;
+ numeroIntem.dataset.id = item.id
  
  novoItem.appendChild(numeroIntem);
  novoItem.innerHTML += item.nome;
+ novoItem.appendChild(botaoDeleta(item.id));
 
  lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+    console.log(document.querySelector(`[data-id='${item.id}']`).innerHTML = item.quantidade);
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "x";
+    elementoBotao.addEventListener('click', function() {
+        deleta(this.parentNode, id);
+    })
+    return elementoBotao
+}
+
+function deleta(tag, id){
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id === id, 1));
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
