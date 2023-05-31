9e3cb7ca4c928d65898a5365d5d794c2af496602
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-05-31 09:23:48 -0300

    Versão com form

--- React/organograma/src/componentes/CampoTexto/index.js
+++ React/organograma/src/componentes/CampoTexto/index.js
@@ -1,10 +1,16 @@
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
+
+    const aoDigitado = (evento) => {
+       let valor = console.log(evento.target.value);
+        console.log(valor)
+    }
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 
