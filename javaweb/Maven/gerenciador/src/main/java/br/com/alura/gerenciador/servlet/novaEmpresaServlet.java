de2744addf1adec71396aa1f3f2f0102d05872af
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-06-13 17:57:19 -0300

    Usando o spring

--- javaweb/Maven/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/novaEmpresaServlet.java
+++ javaweb/Maven/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/novaEmpresaServlet.java
@@ -3,15 +3,16 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.Date;
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 /**
  * Servlet implementation class novaEmpresaServlet
  */
