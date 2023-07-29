56c34333f810e31caf1ce56d7e5e569dd7b45247
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-07-29 11:43:42 -0300

    atualizando

--- Angular/memoteca/src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ Angular/memoteca/src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
+  listaPensamentos = [];
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ Angular/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  pensamento = {
+
+    conteudo: "I love Angular",
+    autoria: "Gabryel",
+    modelo: "modelo2"
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
